9f9465053f55589517fcd19ee612e4bf8849edca
Charles <charleslanop@gmail.com>
2022-02-09 22:32:56 -0300

    added terms-of-service page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import NotFoundPage from '../shared/pages/NotFoundPage';
 import React from 'react';
 import RegisterPage from '../modules/register/pages/RegisterPage';
 import ScrollToTop from '../shared/components/ScrollToTop';
+import TermsOfService from '../shared/pages/TermsOfService';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function index() {
@@ -13,6 +14,7 @@ function index() {
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
+        <Route path='/terms-of-service' element={<TermsOfService />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
       <ScrollToTop />
---
+++ src/shared/pages/TermsOfService.tsx
@@ -0,0 +1,33 @@
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import NavBarOff from '../components/NavBarOff';
+import React from 'react';
+
+function TermsOfService() {
+  return (
+    <>
+      <Header />
+      <NavBarOff />
+      <main>
+        <h1 className='text-center'>Regras e Termos de Serviços</h1>
+        <h2>Regras</h2>
+        <ol>
+          <li>Os jogadores não têm direito a uma conta de jogo.</li>
+          <li>
+            Os jogadores devem aceitar as instruções dadas pelo administrador do
+            jogo ou moderadores do fórum.
+          </li>
+          <li>
+            Todos os bugs (defeitos resultantes do cronograma do jogo) devem ser
+            reportados à equipe imediatamente. O uso de um bug conhecido
+            sujeitará o jogador envolvido ao banimento e todas as contas
+            envolvidas no uso do bug.
+          </li>
+        </ol>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default TermsOfService;
