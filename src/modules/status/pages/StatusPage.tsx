0e71c820702082579628ec037f83b3ab68f5c892
Charles <charleslanop@gmail.com>
2022-02-11 12:00:43 -0300

    updated status page

--- src/modules/status/pages/StatusPage.tsx
+++ src/modules/status/pages/StatusPage.tsx
@@ -6,7 +6,7 @@ import modalService from '../../../shared/services/ModalService';
 import React, { useEffect } from 'react';
 import routes from '../../../routes/routes';
 import userDetailsService from '../services/UserDetailsService';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function StatusPage() {
   const { showLoading, hideLoading } = loaderService();
@@ -14,7 +14,7 @@ function StatusPage() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    requestUserDetails();
+    //requestUserDetails();
   }, []);
 
   const requestUserDetails = async () => {
@@ -34,8 +34,39 @@ function StatusPage() {
     <>
       <Header />
       <MenuDropdown />
-      <main>
-        <h1>Estatísticas</h1>
+      <main className='content'>
+        <div className='alert'>
+          <p>Os lobisomens venceram a Tormentus War - O Saque Infernal.</p>
+          <p>
+            Ambas as raças estão recebendo o dobro de ouro em combates contra a
+            lista negra e 5% a mais de ouro por terem participado da guerra.
+          </p>
+          <p>
+            O bônus vale para os Lobisomens até 11 de Fevereiro de 2022 às
+            02:35.
+          </p>
+          <p>
+            O bônus vale para os Vampiros até 10 de Fevereiro de 2022 às 23:35.
+          </p>
+        </div>
+        <div className='alert'>
+          <p>Os seguintes bosses estão desafiando este mundo:</p>
+          <br />
+          <p>
+            <Link to={routes.status}>Draugr - soldier</Link>
+          </p>
+          <p>
+            <Link to={routes.status}>First Mother - elder</Link>
+          </p>
+        </div>
+        <h1 className='title text-center'>Estatísticas</h1>
+        <p className='sub-title text-center'>Amaldiçoado Comum</p>
+        <span className='text-center uppercase'>
+          Transformou-se em vampiro em 25 de Dezembro de 2021 às 14:25
+        </span>
+        <span className='text-center'>
+          Solteiro(a) <span className='icon-marry-search'></span>
+        </span>
       </main>
       <Footer />
     </>
