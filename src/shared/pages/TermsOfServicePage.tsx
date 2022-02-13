8b27fa12f8ba18c508def8de2eb3704b71341cff
Charles <charleslanop@gmail.com>
2022-02-13 10:52:37 -0300

    updated terms of service page

--- src/shared/pages/TermsOfServicePage.tsx
+++ src/shared/pages/TermsOfServicePage.tsx
@@ -15,13 +15,291 @@ function TermsOfServicePage() {
           <li>Os jogadores não têm direito a uma conta de jogo.</li>
           <li>
             Os jogadores devem aceitar as instruções dadas pelo administrador do
-            jogo ou moderadores do fórum.
+            jogo.
           </li>
           <li>
             Todos os bugs (defeitos resultantes do cronograma do jogo) devem ser
             reportados à equipe imediatamente. O uso de um bug conhecido
             sujeitará o jogador envolvido ao banimento e todas as contas
             envolvidas no uso do bug.
+          </li>
+          <li>
+            Múltiplas contas (ter mais de 1 conta por servidor) e qualquer outra
+            forma de trapaça é proibida e qualquer jogador do jogo envolvido em
+            violação dessas regras está sujeito a banimento imediato. Farming
+            (que envolve atacar multi-contas ou contas de outros jogadores que
+            os preparam para ataques posteriores com seu conhecimento prévio)
+            resultará em banimento e exclusão da conta SEM qualquer aviso
+            prévio.
+          </li>
+          <li>
+            O uso de scripts, bots e outros programas similares para automatizar
+            ações no jogo resultará no banimento do jogador em questão. Scripts
+            / Bots incluem programas que executam ações automáticas no jogo de
+            qualquer tipo ou com controle manual do jogador. Tais ações incluem
+            a leitura de dados diretamente do Game Server ou das páginas que
+            compõem o jogo.
+          </li>
+          <li>
+            Contas de jogadores que insultem outros jogadores ou membros da
+            equipe Lamentosa podem ser banidas ou bloqueadas sem aviso prévio.
+          </li>
+          <li>
+            Os jogadores não estão autorizados a leiloar partes do jogo como
+            contas ou clãs, as contas envolvidas serão banidas.
+          </li>
+          <li>
+            Contas de jogadores que entrarem em contas de outros jogadores sem
+            autorização ou que tenham contas de jogadores roubadas serão
+            banidas.
+          </li>
+          <li>
+            Contas contendo apelidos ofensivos, pornográficos, ilegais ou
+            politicamente extremistas serão banidas.
+          </li>
+          <li>
+            As contas de jogadores que enviarem mensagens ofensivas,
+            pornográficas, ilegais ou politicamente extremistas no jogo, ou que
+            contenham tal conteúdo no perfil (seja por texto ou imagens) serão
+            banidas.
+          </li>
+          <li>
+            É proibido enviar spam ou deliberadamente irritar ou provocar outros
+            jogadores através do jogo. O jogador será avisado se recebermos
+            reclamações de outros jogadores e as contas de jogadores que
+            ofenderem repetidamente serão bloqueadas ou banidas! Ataques
+            repetidos não são uma forma de assédio/assédio!
+          </li>
+          <li>
+            É proibida a distribuição de quaisquer ferramentas/bots/certificados
+            destinados ao manuseio do Lamentosa e resultará na exclusão dos
+            clientes do jogo envolvidos.
+          </li>
+        </ol>
+        <h2>Termos de Serviços</h2>
+        <ol>
+          <li>
+            Notas preliminares
+            <ul>
+              <li>
+                1.1 As seguintes disposições referem-se ao jogo online Lamentosa
+                e seus derivados. Ao utilizar quaisquer serviços prestados pela
+                Lamentosa, o Usuário declara que leu e aceitou os Termos de
+                Serviço. O registo em Lamentosa não é possível se o Utilizador
+                não tiver expressamente concordado em aderir aos Termos de
+                Serviço.
+              </li>
+              <li>
+                1.2 Estas disposições regulam a utilização dos serviços
+                prestados pela Lamentosa e destinam-se a garantir o mais alto
+                nível de qualidade e evitar abusos. Estas disposições são a base
+                para a venda e fornecimento de bens e serviços através do jogo
+                online Lamentosa.
+              </li>
+              <li>
+                1.3 O jogo Lamentosa está disponível para Usuários em todo o
+                mundo e pode, em alguns casos, ter versões específicas em seu
+                idioma.
+              </li>
+            </ul>
+          </li>
+          <br />
+          <li>
+            Serviços e Conteúdos de Lamentosa (doravante denominados "serviços")
+            <ul>
+              <li>Lamentosa é um jogo de RPG multiplayer online.</li>
+              <li>
+                2.1 Ao registar-se no Lamentosa, o Utilizador tem o direito de
+                aceder ao Lamentosa em qualquer servidor para o qual se tenha
+                registado.
+              </li>
+              <li>
+                2.2. A Lamentosa disponibiliza um conjunto de links que conduzem
+                a plataformas de vendas na Internet de outras empresas. Caso os
+                Utilizadores do Lamentosa optem por utilizar ou adquirir bens
+                e/ou serviços a algum destes terceiros, aplicam-se as Condições
+                de Venda das partes envolvidas. A Lamentosa apenas fornece
+                acesso à plataforma de vendas desses terceiros e não está
+                envolvida em nenhuma venda.
+              </li>
+            </ul>
+          </li>
+          <br />
+          <li>
+            Obrigações dos Usuários
+            <ul>
+              <li>
+                3.1 Os Utilizadores estão obrigados a utilizar legalmente o
+                Lamentosa.
+              </li>
+              <li>
+                3.2 O Usuário garante que todas as informações fornecidas no
+                processo de registro estão corretas e atualizadas. A Lamentosa
+                será imediatamente informada de quaisquer alterações
+                relacionadas com esta informação, efetuadas após o registo. Caso
+                o Utilizador não o faça, é totalmente responsável pelos danos ou
+                inconvenientes daí resultantes.
+              </li>
+              <li>
+                3.3 O Usuário garante que o nome de usuário e a senha foram
+                cuidadosamente escolhidos e não foram divulgados a terceiros.
+                Caso o Usuário não cumpra esta obrigação, seja de forma
+                consciente ou negligente, será responsável por todo e qualquer
+                dano resultante do abuso da senha por terceiros.
+              </li>
+              <li>
+                3.4 O Usuário concorda em abster-se de divulgar ou
+                disponibilizar qualquer material ilegal ou protegido por
+                direitos autorais em qualquer local do Jogo Lamentosa
+                (mensagens, descrição in-game). É expressamente proibida a
+                distribuição ou publicação de material racista ou extremista,
+                pornografia infantil ou qualquer material que possa pôr em
+                perigo o bem-estar ou a saúde das crianças. A administração
+                reserva-se o direito de remover as descrições de personagens e
+                clãs se o material for ilegal, obsceno, extremista, de mau gosto
+                ou que perturbe o curso normal de comunicação no meio envolvido.
+              </li>
+              <li>
+                3.5 Caso a Lamentosa perceba ou seja informada por terceiros de
+                atividades ilícitas do(s) Usuário(s), a Lamentosa fornecerá às
+                autoridades competentes toda e qualquer informação pessoal sobre
+                o(s) Usuário(s) exigida por lei. A Lamentosa cooperará com
+                qualquer consulta sobre a legalidade da transferência de
+                informações sobre o Usuário.
+              </li>
+              <li>
+                3.6 O Usuário concorda em transferir os direitos de qualquer
+                material postado ou copiado no site Lamentosa (descrições,
+                mensagens, etc.) O Usuário concorda em indenizar a Lamentosa de
+                toda e qualquer reclamação por danos decorrentes de
+                descumprimento por parte do Usuário que resulte em violação de
+                direitos autorais ou de propriedade. A Lamentosa reserva-se o
+                direito de modificar ou remover qualquer conteúdo postado pelos
+                Usuários sem mencionar o motivo da remoção.
+              </li>
+              <li>
+                3.7 O Usuário indeniza a Lamentosa de toda e qualquer reclamação
+                por danos contra a Lamentosa decorrentes de ações do Usuário ou
+                de qualquer terceiro agindo em nome do Usuário.
+              </li>
+            </ul>
+          </li>
+          <br />
+          <li>
+            Responsabilidade
+            <ul>
+              <li>
+                4.1 A Lamentosa não pode garantir a disponibilidade ou
+                funcionamento de 100% dos serviços prestados. A Lamentosa não se
+                responsabiliza por quaisquer danos decorrentes da
+                indisponibilidade dos serviços, qualquer que seja o fundamento
+                legal do pedido de indemnização.
+              </li>
+              <li>
+                4.2 O Lamentosa só se responsabiliza pela postagem ou publicação
+                de material ilegal ou protegido por direitos autorais, no jogo,
+                se o Lamentosa estava ciente ou informado sobre o material e não
+                tomou nenhuma medida para evitar sua disseminação , quando for
+                técnica e financeiramente possível evitar o uso continuado do
+                material.
+              </li>
+              <li>
+                4.3 A Lamentosa não se responsabiliza pela exatidão, legalidade,
+                integridade ou recuperabilidade de quaisquer ofertas feitas por
+                terceiros. Além disso, Lamentosa não assume responsabilidade
+                pela integridade e precisão de qualquer conteúdo que tenha sido
+                postado no site Lamentosa por terceiros.
+              </li>
+              <li>
+                4.4 A Lamentosa não se responsabiliza por conteúdos de terceiros
+                para os quais a Lamentosa atua apenas como motorista. No
+                entanto, Lamentosa pode ser legalmente responsável por bloquear
+                a divulgação de material ilegal ou protegido por direitos
+                autorais quando a distribuição do material violar as leis
+                aplicáveis.
+              </li>
+            </ul>
+          </li>
+          <br />
+          <li>
+            Proteção de dados
+            <ul>
+              <li>
+                5.1 O Usuário aceita que todas as informações que você inserir
+                nas páginas do Lamentosa possam ser registradas e processadas,
+                caso essas ações sejam necessárias para manter os Termos de
+                Serviço.
+              </li>
+              <li>
+                5.2 A Lamentosa trata todos os dados do Utilizador de forma
+                confidencial e com a autorização do Utilizador. Qualquer uso dos
+                dados pessoais do Usuário além do aqui descrito só ocorrerá após
+                autorização expressa do Usuário.
+              </li>
+              <li>
+                5.3 Os dados registrados serão arquivados apenas enquanto for
+                exigido pelos Termos de Serviço, e o período de arquivamento
+                corresponde a quaisquer leis aplicáveis.
+              </li>
+              <li>
+                5.4 O Utilizador foi então totalmente informado da natureza,
+                extensão, localização e finalidade da recolha, utilização e
+                tratamento dos dados pessoais necessários para a execução dos
+                Serviços. O Usuário concorda e aceita integralmente a coleta e
+                processamento desses dados.
+              </li>
+            </ul>
+          </li>
+          <br />
+          <li>
+            Regulamento da Atividade do Jogo
+            <ul>
+              <li>
+                6.1 Todos os conteúdos e serviços oferecidos pela Lamentosa se
+                enquadram nas regras do jogo (ver acima), as quais o Usuário
+                concordou em cumprir.
+              </li>
+            </ul>
+          </li>
+          <br />
+          <li>
+            Cláusula Final
+            <ul>
+              <li>
+                7.1 O conteúdo e os Serviços oferecidos pela Lamentosa estão
+                protegidos pela lei de direitos autorais. Você pode baixar e
+                imprimir este conteúdo em seu computador pessoal. O Usuário não
+                está autorizado a copiar, vender, publicar ou distribuir o
+                conteúdo encontrado em Lamentosa.
+              </li>
+              <li>
+                7.2 A Lamentosa reserva-se o direito de alterar ou complementar
+                os Termos de Serviço. Você concorda em revisar regularmente as
+                Regras do Jogo e os Termos de Serviço para detectar quaisquer
+                alterações. Ao usar os Serviços fornecidos, o Usuário concorda
+                automaticamente em cumprir as Regras do Jogo e os Termos de
+                Serviço aplicáveis.
+              </li>
+              <li>
+                7.3 Os Termos de Serviço e o relacionamento entre você e
+                Lamentosa serão regidos pelas leis do Brasil.
+              </li>
+              <li>
+                7.4 Todas as partes envolvidas concordam em submeter-se à
+                jurisdição pessoal e exclusiva dos tribunais localizados no
+                Brasil.
+              </li>
+              <li>
+                7.5 Qualquer disputa decorrente do uso dos Serviços prestados
+                pela Lamentosa ou relacionada aos Termos de Serviço são
+                resolvidas exclusivamente pelos tribunais do Brasil.
+              </li>
+              <li>
+                7.6 Se qualquer condição estabelecida nos Termos de Serviço for
+                ou se tornar inválida, todas as outras condições permanecerão
+                totalmente aplicáveis.
+              </li>
+            </ul>
           </li>
         </ol>
       </main>
