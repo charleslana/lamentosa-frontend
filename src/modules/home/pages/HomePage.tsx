998a68fa10c31b6e2474840b62d18076b446ce59
Charles <charleslanop@gmail.com>
2022-02-21 18:54:55 -0300

    add translate

--- src/modules/home/pages/HomePage.tsx
+++ src/modules/home/pages/HomePage.tsx
@@ -2,11 +2,15 @@ import Footer from '../../../shared/components/Footer';
 import Header from '../../../shared/components/Header';
 import ITopPlayersVampires from '../interfaces/ITopPlayersVampires';
 import NavBar from '../../../shared/components/NavBar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import translate from '../../../translate/translate';
 import { Link } from 'react-router-dom';
 
 function HomePage() {
-  const topPlayersVampires: ITopPlayersVampires[] = [
+  const [topPlayersVampires, setTopPlayersVampires] =
+    useState<ITopPlayersVampires[]>();
+
+  const dataTopPlayersVampires: ITopPlayersVampires[] = [
     {
       name: 'CharlesLanaMarquesD1',
       color: 0,
@@ -63,6 +67,14 @@ function HomePage() {
     },
   ];
 
+  useEffect(() => {
+    requestHome();
+  }, []);
+
+  const requestHome = async () => {
+    setTopPlayersVampires(dataTopPlayersVampires);
+  };
+
   const validateBar = (min: number, max: number): number => {
     const total = (min / max) * 115;
     if (total > 115) return 115;
@@ -74,107 +86,41 @@ function HomePage() {
       <Header />
       <NavBar />
       <main>
-        <h1 className='text-center'>Jogo de Vampiro</h1>
-        <p>
-          Lamentosa é um jogo de vampiro online onde você compete contra
-          centenas de jogadores em busca de poder e influência. É preciso saber
-          fazer política e conquistar aliados pois aqui você não vive sozinho.
-        </p>
+        <h1 className='text-center'>{translate.home.title}</h1>
+        <p>{translate.home.detail1}</p>
         <br />
-        <p>
-          Use o chat ao seu favor já que este jogo não se resume em apenas
-          evoluir seu personagem, é necessário criar laços, entrar em clãs,
-          construir estratégias de ataque em massa e muito mais.
-        </p>
+        <p>{translate.home.detail2}</p>
         <br />
-        <p>
-          O jogo roda diretamente no seu navegador, não é preciso baixar ou
-          instalar nada. Temos mais de 25000 jogadores cadastrados!
-        </p>
+        <p>{translate.home.detail3}</p>
         <Link to='/register'>
-          <button className='m-auto'>Jogue Agora!</button>
+          <button className='m-auto'>{translate.home.button}</button>
         </Link>
-        <h2>Jogo de Vampiro Real</h2>
-        <p>
-          Lamentosa vai desafiar a sua mente mais do que qualquer outro rpg de
-          terror que tenha visto por aí. Você fará muitos amigos e também
-          inimigos… É preciso ser implacável para sobreviver.
-        </p>
+        <h2>{translate.home.subTitle1}</h2>
+        <p>{translate.home.detail4}</p>
         <br />
-        <p>
-          Os vampiros mais poderosos de clãs que não devemos sequer falar o
-          nome, estão cegos pelo poder, você pode crescer aos poucos e com o
-          tempo tomar o lugar destes vampiros anciãos.
-        </p>
+        <p>{translate.home.detail5}</p>
         <br />
-        <p>
-          A história de Lamentosa é bastante longa e difusa, para entender o
-          contexto você deve ir até a biblioteca de algum clã antigo, em
-          seguida, deve pedir autorização à um vampiro líder para que possa ter
-          acesso aos livros ocultos, neles é possível obter o conhecimento dos
-          mestres.
-        </p>
-        <h2>Escolha um mestre Vampiro</h2>
-        <p>
-          A partir do nível 10 você deve escolher um mestre para guiá-lo em sua
-          jornada, cada mestre ensinará habilidades distintas, portanto, é
-          preciso pensar com cautela. Dizem que os mestres podem eliminar todos
-          os vampiros do mundo em um piscar de olhos, tamanho é o poder dessas
-          criaturas!
-        </p>
+        <p>{translate.home.detail6}</p>
+        <h2>{translate.home.subTitle2}</h2>
+        <p>{translate.home.detail7}</p>
         <br />
-        <p>
-          Outro ponto muito importante neste jogo de vampiro são as batalhas
-          coletivas em tempo real. Uma vez por dia os mestres vão tocar os
-          tambores de guerra, neste momento, todos os vampiros que estiverem
-          online devem ir ao desfiladeiro Tormentus para uma sangrenta batalha
-          em grupo, ao final da batalha um excelente prêmio será dado.
-        </p>
+        <p>{translate.home.detail8}</p>
         <br />
-        <p>
-          Existem vários outros eventos coletivos em Lamentosa, como por
-          exemplo, a guerra dos clãs que incluem sabotagens e magias. Você pode
-          também diariamente atacar um Boss em grupo e obter itens exclusivos,
-          afinal, vampiros precisam estar bem equipados em tempos de guerra!
-        </p>
-        <h2>Revele-se como um Vampiro estrategista!</h2>
-        <p>
-          Não seja tímido! Como já foi dito, é preciso fazer alianças, então
-          participe dos grupos que for convidado no WhatsApp, telegram ou
-          discord. Além disso empenhe-se em socializar no chat do jogo de
-          vampiro.
-        </p>
+        <p>{translate.home.detail9}</p>
+        <h2>{translate.home.subTitle3}</h2>
+        <p>{translate.home.detail10}</p>
         <br />
-        <p>
-          Quando falei sobre saber fazer política eu não estava brincando! A
-          medida que você for se destacando no jogo será natural que outros
-          vampiros se aproximem de você para viverem à sua sombra, seja por
-          proteção, para ganhar algum item ou algum conhecimento sobre magias
-          ocultas, enfim, é necessário ter um olho clínico para abordagens
-          assim, e se você for um bom estrategista conseguirá apoio para um
-          excelente objetivo: Tornar-se o grande líder dos vampiros. No dia 3 de
-          cada mês acontece uma eleição onde os vampiros elegem o grande líder
-          para os próximos 30 dias. O líder tem um papel de destaque nas
-          decisões da raça, ele recebe itens Tenebris (muito fortes e
-          indestrutíveis) e pode expulsar outros vampiros de um campo de
-          batalha, por tudo isso, é um cargo bastante visado.
-        </p>
+        <p>{translate.home.detail11}</p>
         <br />
-        <p>
-          Bom, em resumo é isso que você precisa saber agora, pois se fosse
-          falar tudo o que se pode fazer aqui, todas as possibilidades
-          estratégicas, builds, etc ficaria um texto enorme e muito chato. Outro
-          ponto é que queremos que você se socialize com os outros jogadores
-          vampiros para tirar suas dúvidas :)
-        </p>
+        <p>{translate.home.detail12}</p>
         <br />
-        <p>E então, vamos começar a jogar Lamentosa agora?</p>
+        <p>{translate.home.detail13}</p>
         <Link to='/register'>
-          <button className='m-auto'>Jogue Agora!</button>
+          <button className='m-auto'>{translate.home.button}</button>
         </Link>
-        <h2>Alguns Vampiros que jogam lamentosa</h2>
+        <h2>{translate.home.subTitle4}</h2>
         <div className='tops-vampire'>
-          {topPlayersVampires.map(vampire => (
+          {topPlayersVampires?.map(vampire => (
             <div key={vampire.name} className='frame-character'>
               <div
                 className={`frame-avatar avatar-vampire-${vampire.gender}-${vampire.avatar}`}
---
+++ src/translate/translate.ts
@@ -0,0 +1,72 @@
+import LocalizedStrings from 'react-localization';
+
+const translate = new LocalizedStrings({
+  en: {
+    home: {
+      title: 'Vampire Game',
+      detail1: `Lamentosa is an online vampire game where you compete against hundreds of players for power and influence. You need to know how to do politics and win allies because here you don't live alone.`,
+      detail2:
+        'Use chat to your advantage as this game is not just about evolving your character, it is necessary to create bonds, join clans, build mass attack strategies and much more.',
+      detail3:
+        'The game runs directly in your browser, there is no need to download or install anything. We have more than 25000 registered players!',
+      button: 'Play now!',
+      subTitle1: 'Royal Vampire Game',
+      detail4: `Lamentable will challenge your mind more than any other horror rpg you've seen out there. You will make many friends as well as enemies… You have to be relentless to survive.`,
+      detail5:
+        'The most powerful vampires from clans we must not even name are blinded by power, you can grow little by little and in time take the place of these elder vampires.',
+      detail6: `Lamentosa's history is quite long and diffuse, to understand the context you must go to the library of some ancient clan, then you must ask a vampire leader for permission so that you can have access to the hidden books, in which it is possible to obtain knowledge. of the masters.`,
+      subTitle2: 'Choose a Vampire Master',
+      detail7:
+        'Starting at level 10 you must choose a master to guide you on your journey, each master will teach you different skills, so you need to think carefully. It is said that the masters can eliminate all the vampires in the world in the blink of an eye, such is the power of these creatures!',
+      detail8:
+        'Another very important point in this vampire game is the collective battles in real time. Once a day the masters will beat the war drums, at this time, all vampires who are online must go to Tormentus Gorge for a bloody group battle, at the end of the battle an excellent prize will be given.',
+      detail9:
+        'There are several other collective events in Lamentosa, such as the clan wars that include sabotage and spells. You can also daily attack a Boss in a group and get exclusive items, after all, vampires need to be well equipped in times of war!',
+      subTitle3: 'Reveal yourself as a Vampire strategist!',
+      detail10:
+        'Do not be shy! As already said, alliances are necessary, so join the groups that you are invited on WhatsApp, telegram or discord. Also, engage in socializing in the vampire game chat.',
+      detail11: `When I talked about knowing how to do politics, I wasn't kidding! As you stand out in the game, it will be natural for other vampires to approach you to live in your shadow, whether for protection, to gain some item or some knowledge about occult magic, in short, it is necessary to have a keen eye for approaches like this. , and if you are a good strategist, you will get support for an excellent goal: to become the great leader of the vampires. On the 3rd of each month an election takes place where the vampires elect the great leader for the next 30 days. The leader has a prominent role in the decisions of the race, he receives Tenebris items (very strong and indestructible) and can expel other vampires from a battlefield, for all this, it is a very sought after position.`,
+      detail12:
+        'Well, in short, this is what you need to know now, because if I were to talk about everything that can be done here, all the strategic possibilities, builds, etc., it would be a huge and very boring text. Another point is that we want you to socialize with other vampire players to clear your doubts :)',
+      detail13: 'So, shall we start playing Lamenta now?',
+      subTitle4: 'Some Vampires that play mournful',
+    },
+  },
+  pt: {
+    home: {
+      title: 'Jogo de Vampiro',
+      detail1:
+        'Lamentosa é um jogo de vampiro online onde você compete contra centenas de jogadores em busca de poder e influência. É preciso saber fazer política e conquistar aliados pois aqui você não vive sozinho.',
+      detail2:
+        'Use o chat ao seu favor já que este jogo não se resume em apenas evoluir seu personagem, é necessário criar laços, entrar em clãs, construir estratégias de ataque em massa e muito mais.',
+      detail3:
+        'O jogo roda diretamente no seu navegador, não é preciso baixar ou instalar nada. Temos mais de 25000 jogadores cadastrados!',
+      button: 'Jogue Agora!',
+      subTitle1: 'Jogo de Vampiro Real',
+      detail4:
+        'Lamentosa vai desafiar a sua mente mais do que qualquer outro rpg de terror que tenha visto por aí. Você fará muitos amigos e também inimigos… É preciso ser implacável para sobreviver.',
+      detail5:
+        'Os vampiros mais poderosos de clãs que não devemos sequer falar o nome, estão cegos pelo poder, você pode crescer aos poucos e com o tempo tomar o lugar destes vampiros anciãos.',
+      detail6:
+        'A história de Lamentosa é bastante longa e difusa, para entender o contexto você deve ir até a biblioteca de algum clã antigo, em seguida, deve pedir autorização à um vampiro líder para que possa ter acesso aos livros ocultos, neles é possível obter o conhecimento dos mestres.',
+      subTitle2: 'Escolha um mestre Vampiro',
+      detail7:
+        'A partir do nível 10 você deve escolher um mestre para guiá-lo em sua jornada, cada mestre ensinará habilidades distintas, portanto, é preciso pensar com cautela. Dizem que os mestres podem eliminar todos os vampiros do mundo em um piscar de olhos, tamanho é o poder dessas criaturas!',
+      detail8:
+        'Outro ponto muito importante neste jogo de vampiro são as batalhas coletivas em tempo real. Uma vez por dia os mestres vão tocar os tambores de guerra, neste momento, todos os vampiros que estiverem online devem ir ao desfiladeiro Tormentus para uma sangrenta batalha em grupo, ao final da batalha um excelente prêmio será dado.',
+      detail9:
+        'Existem vários outros eventos coletivos em Lamentosa, como por exemplo, a guerra dos clãs que incluem sabotagens e magias. Você pode também diariamente atacar um Boss em grupo e obter itens exclusivos, afinal, vampiros precisam estar bem equipados em tempos de guerra!',
+      subTitle3: 'Revele-se como um Vampiro estrategista!',
+      detail10:
+        'Não seja tímido! Como já foi dito, é preciso fazer alianças, então participe dos grupos que for convidado no WhatsApp, telegram ou discord. Além disso empenhe-se em socializar no chat do jogo de vampiro.',
+      detail11:
+        'Quando falei sobre saber fazer política eu não estava brincando! A medida que você for se destacando no jogo será natural que outros vampiros se aproximem de você para viverem à sua sombra, seja por proteção, para ganhar algum item ou algum conhecimento sobre magias ocultas, enfim, é necessário ter um olho clínico para abordagens assim, e se você for um bom estrategista conseguirá apoio para um excelente objetivo: Tornar-se o grande líder dos vampiros. No dia 3 de cada mês acontece uma eleição onde os vampiros elegem o grande líder para os próximos 30 dias. O líder tem um papel de destaque nas decisões da raça, ele recebe itens Tenebris (muito fortes e indestrutíveis) e pode expulsar outros vampiros de um campo de batalha, por tudo isso, é um cargo bastante visado.',
+      detail12:
+        'Bom, em resumo é isso que você precisa saber agora, pois se fosse falar tudo o que se pode fazer aqui, todas as possibilidades estratégicas, builds, etc ficaria um texto enorme e muito chato. Outro ponto é que queremos que você se socialize com os outros jogadores vampiros para tirar suas dúvidas :)',
+      detail13: 'E então, vamos começar a jogar Lamentosa agora?',
+      subTitle4: 'Alguns Vampiros que jogam lamentosa',
+    },
+  },
+});
+
+export default translate;
