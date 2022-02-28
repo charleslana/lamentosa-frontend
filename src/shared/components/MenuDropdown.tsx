cab7186806974d5d509624cb774c480d544076f3
Charles <charleslanop@gmail.com>
2022-02-28 12:44:07 -0300

    update translate

--- src/shared/components/MenuDropdown.tsx
+++ src/shared/components/MenuDropdown.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import routes from '../../routes/routes';
+import translate from '../../translate/translate';
 import { Link } from 'react-router-dom';
 
 function MenuDropdown() {
@@ -26,96 +27,152 @@ function MenuDropdown() {
       <div className={`menu-dropdown ${menu ? 'd-block' : ''}`}>
         <div className='dropdown'>
           <button className='dropdown-link'>
-            Estatísticas<span className='arrow-down'></span>
+            {translate.menuDropdown.statistics}
+            <span className='arrow-down'></span>
           </button>
           <div className='dropdown-content'>
-            <Link to={routes.status}>Meu Personagem</Link>
-            <Link to={routes.grimoire}>Meu Grimório</Link>
-            <Link to={routes.sentinels}>Minhas Sentinelas</Link>
-            <Link to={routes.dailyTasks}>Tarefas Diárias</Link>
-            <Link to={routes.achievements}>Conquistas</Link>
-            <Link to={routes.exchangeStones}>Troca de Soul Stones</Link>
-            <Link to={routes.honorPoints}>Pontos de Honra</Link>
+            <Link to={routes.status}>{translate.menuDropdown.status}</Link>
+            <Link to={routes.grimoire}>{translate.menuDropdown.grimoire}</Link>
+            <Link to={routes.sentinels}>
+              {translate.menuDropdown.sentinels}
+            </Link>
+            <Link to={routes.dailyTasks}>
+              {translate.menuDropdown.dailyTasks}
+            </Link>
+            <Link to={routes.achievements}>
+              {translate.menuDropdown.achievements}
+            </Link>
+            <Link to={routes.exchangeStones}>
+              {translate.menuDropdown.exchangeStones}
+            </Link>
+            <Link to={routes.honorPoints}>
+              {translate.menuDropdown.honorPoints}
+            </Link>
           </div>
         </div>
         <div className='dropdown'>
           <button className='dropdown-link'>
-            Cidade<span className='arrow-down'></span>
+            {translate.menuDropdown.city}
+            <span className='arrow-down'></span>
           </button>
           <div className='dropdown-content'>
-            <Link to={routes.status}>O Mercado</Link>
-            <Link to={routes.status}>Mercado Premium</Link>
-            <Link to={routes.status}>O Ferreiro</Link>
-            <Link to={routes.status}>A Feiticeira</Link>
-            <Link to={routes.status}>Cemitério</Link>
-            <Link to={routes.status}>Templo</Link>
-            <Link to={routes.status}>Meu Mestre</Link>
-            <Link to={routes.status}>As 7 Regiões</Link>
+            <Link to={routes.market}>{translate.menuDropdown.market}</Link>
+            <Link to={routes.premiumMarket}>
+              {translate.menuDropdown.premiumMarket}
+            </Link>
+            <Link to={routes.blacksmith}>
+              {translate.menuDropdown.blacksmith}
+            </Link>
+            <Link to={routes.enchantment}>
+              {translate.menuDropdown.enchantment}
+            </Link>
+            <Link to={routes.cemetery}>{translate.menuDropdown.cemetery}</Link>
+            <Link to={routes.temple}>{translate.menuDropdown.temple}</Link>
+            <Link to={routes.master}>{translate.menuDropdown.master}</Link>
+            <Link to={routes.regions}>{translate.menuDropdown.regions}</Link>
           </div>
         </div>
         <div className='dropdown'>
           <button className='dropdown-link'>
-            Campo de Batalha<span className='arrow-down'></span>
+            {translate.menuDropdown.battlefield}
+            <span className='arrow-down'></span>
           </button>
           <div className='dropdown-content'>
-            <Link to={routes.status}>Caçar Criaturas</Link>
-            <Link to={routes.status}>Caçar lobisomens</Link>
-            <Link to={routes.status}>Caçar Renegados</Link>
-            <Link to={routes.status}>Tormentus War</Link>
-            <Link to={routes.status}>Khepri Arena</Link>
+            <Link to={routes.battlefield}>
+              {translate.menuDropdown.battlefieldCreatures}
+            </Link>
+            <Link to={routes.battlefieldEnemies}>
+              {translate.menuDropdown.battlefieldEnemies}
+            </Link>
+            <Link to={routes.battlefieldRenegades}>
+              {translate.menuDropdown.battlefieldRenegades}
+            </Link>
+            <Link to={routes.tormentus}>
+              {translate.menuDropdown.tormentus}
+            </Link>
+            <Link to={routes.khepriArena}>
+              {translate.menuDropdown.khepriArena}
+            </Link>
           </div>
         </div>
         <div className='dropdown'>
           <button className='dropdown-link'>
-            Clãs<span className='arrow-down'></span>
+            {translate.menuDropdown.clan}
+            <span className='arrow-down'></span>
           </button>
           <div className='dropdown-content'>
-            <Link to={routes.status}>Meu Clã</Link>
-            <Link to={routes.status}>Todos os Clãs</Link>
-            <Link to={routes.status}>Criar um Clã</Link>
+            <Link to={routes.clan}>{translate.menuDropdown.myClan}</Link>
+            <Link to={routes.clanList}>{translate.menuDropdown.clanList}</Link>
+            <Link to={routes.clanCrete}>
+              {translate.menuDropdown.createClan}
+            </Link>
           </div>
         </div>
         <div className='dropdown'>
           <button className='dropdown-link'>
-            Jornal<span className='arrow-down'></span>
+            {translate.menuDropdown.journal}
+            <span className='arrow-down'></span>
           </button>
           <div className='dropdown-content'>
-            <Link to={routes.status}>Ranking</Link>
-            <Link to={routes.status}>Placar Tormentus</Link>
-            <Link to={routes.status}>Liderança</Link>
-            <Link to={routes.status}>Itens Únicos</Link>
-            <Link to={routes.status}>Bosses</Link>
-            <Link to={routes.status}>Heróis</Link>
-            <Link to={routes.status}>Renegados</Link>
-            <Link to={routes.status}>Procurar Personagem</Link>
+            <Link to={routes.ranking}>{translate.menuDropdown.ranking}</Link>
+            <Link to={routes.tormentusScore}>
+              {translate.menuDropdown.tormentusScore}
+            </Link>
+            <Link to={routes.leadership}>
+              {translate.menuDropdown.leadership}
+            </Link>
+            <Link to={routes.itemsUnique}>
+              {translate.menuDropdown.itemsUnique}
+            </Link>
+            <Link to={routes.boss}>{translate.menuDropdown.boss}</Link>
+            <Link to={routes.heroes}>{translate.menuDropdown.heroes}</Link>
+            <Link to={routes.renegades}>
+              {translate.menuDropdown.renegades}
+            </Link>
+            <Link to={routes.searchPlayer}>
+              {translate.menuDropdown.searchPlayer}
+            </Link>
           </div>
         </div>
         <div className='dropdown'>
-          <Link to={routes.status}>
-            <button className='dropdown-link'>Mensageiro</button>
+          <Link to={routes.messages}>
+            <button className='dropdown-link'>
+              {translate.menuDropdown.messages}
+            </button>
           </Link>
         </div>
         <div className='dropdown'>
           <button className='dropdown-link'>
-            Ajuda<span className='arrow-down'></span>
+            {translate.menuDropdown.help}
+            <span className='arrow-down'></span>
           </button>
           <div className='dropdown-content'>
-            <Link to={routes.status}>Aprenda a jogar</Link>
-            <Link to={routes.status}>O Manual dos Amaldiçoados</Link>
-            <Link to={routes.status}>Contato</Link>
+            <Link to={routes.manual}>{translate.menuDropdown.manual}</Link>
+            <Link to={routes.contact}>{translate.menuDropdown.contact}</Link>
           </div>
         </div>
         <div className='dropdown'>
           <button className='dropdown-link'>
-            Conta<span className='arrow-down'></span>
+            {translate.menuDropdown.account}
+            <span className='arrow-down'></span>
           </button>
           <div className='dropdown-content'>
-            <Link to={routes.status}>Cristais de Sangue</Link>
-            <Link to={routes.status}>Mudar seu Nome</Link>
-            <Link to={routes.status}>Editar seu perfil</Link>
-            <Link to={routes.status}>Mudar Gênero</Link>
-            <Link to={routes.status}>Mudar de Raça</Link>
-            <Link to={routes.login}>Sair</Link>
+            <Link to={routes.bloodCrystals}>
+              {translate.menuDropdown.bloodCrystals}
+            </Link>
+            <Link to={routes.changeCharacterName}>
+              {translate.menuDropdown.changeCharacterName}
+            </Link>
+            <Link to={routes.editProfile}>
+              {translate.menuDropdown.editProfile}
+            </Link>
+            <Link to={routes.changeGender}>
+              {translate.menuDropdown.changeGender}
+            </Link>
+            <Link to={routes.changeBreed}>
+              {translate.menuDropdown.changeBreed}
+            </Link>
+            <Link to={routes.login}>{translate.menuDropdown.logout}</Link>
           </div>
         </div>
       </div>
