02b9fe39a1a97f17005803b0203c0121a3a47271
Charles <charleslanop@gmail.com>
2022-03-06 13:41:53 -0300

    refactoring

--- src/modules/daily-tasks/components/TabBarDailyTasks.tsx
+++ src/modules/daily-tasks/components/TabBarDailyTasks.tsx
@@ -2,10 +2,11 @@ import CustomModal from '../../../shared/components/CustomModal';
 import Instructions from './Instructions';
 import React, { useState } from 'react';
 import routes from '../../../routes/routes';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function TabBarDailyTasks() {
   const [modal, setModal] = useState(false);
+  const { pathname } = useLocation();
 
   const showInstructions = () => {
     setModal(!modal);
@@ -14,8 +15,20 @@ function TabBarDailyTasks() {
   return (
     <>
       <nav>
-        <Link to={routes.dailyTasks}>Recompensas</Link>
-        <Link to={routes.dailyTasksHistoric}>Histórico</Link>
+        <Link
+          to={routes.dailyTasks}
+          className={pathname === routes.dailyTasks ? 'active' : ''}
+        >
+          Recompensas
+        </Link>
+        <Link
+          to={routes.dailyTasksHistoric}
+          className={
+            pathname.includes(routes.dailyTasksHistoric) ? 'active' : ''
+          }
+        >
+          Histórico
+        </Link>
         <button onClick={showInstructions}>Instruções</button>
       </nav>
       {modal ? (
--- src/shared/components/NavBar.tsx
+++ src/shared/components/NavBar.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import routes from '../../routes/routes';
 import translate from '../../translate/translate';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function NavBar() {
+  const { pathname } = useLocation();
+
   return (
     <nav className='nav-sticky'>
-      <Link to={routes.home}>{translate.navBar.home}</Link>
-      <Link to={routes.login}>{translate.navBar.login}</Link>
-      <Link to={routes.register}>{translate.navBar.register}</Link>
+      <Link
+        to={routes.home}
+        className={pathname === routes.home ? 'active' : ''}
+      >
+        {translate.navBar.home}
+      </Link>
+      <Link
+        to={routes.login}
+        className={pathname.includes(routes.login) ? 'active' : ''}
+      >
+        {translate.navBar.login}
+      </Link>
+      <Link
+        to={routes.register}
+        className={pathname.includes(routes.register) ? 'active' : ''}
+      >
+        {translate.navBar.register}
+      </Link>
     </nav>
   );
 }
